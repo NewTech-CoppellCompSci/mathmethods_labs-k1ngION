47c180bc9f64776e4a28e2a5dc9d0a5328a88adf
k1ngION <agaziyani@newtech.coppellisd.com>
2022-02-28 15:45:54 -0600

    hi sorry for da late submission

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -16,13 +16,15 @@ public class Lab01 {
 
 	
 	public static void problem01() {
-Scanner inKey = new Scanner(System.in);
-System.out.println("Enter a positive interger");
-int usernum = inKey.nextInt();
-System.out.println("Enter another postive integer");
-int usernum1 = inKey.nextInt();
-System.out.println(usernum + "^" + usernum1 + Math.pow(usernum, usernum1));
-
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt()
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		
+		System.out.println(userNum + "^" + Math.pow(userNum, userNum2));
+			
+		
 		
 	}
 	
@@ -30,7 +32,11 @@ System.out.println(usernum + "^" + usernum1 + Math.pow(usernum, usernum1));
 	
 	
 	public static void problem02() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positice integer");
+		int userNum = inKey.nextInt();
 		
+		System.out.println("The square root of " + userNum + "is" + Math.sqrt(userNum));
 		
 		
 	}
@@ -40,8 +46,13 @@ System.out.println(usernum + "^" + usernum1 + Math.pow(usernum, usernum1));
 
 	public static void problem03() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("What is the side A of a right triangle");
+		int userNum = inKey.nextInt();
+		System.out.println("What is the B side of a right triangle");
+		int userNum2 = inKey.nextInt();
 		
-		
+		System.out.println("Hypotenuse =" + Math.sqrt(Math.pow(userNum, 2)+(Math.pow(userNum2, 2))));
 
 	}
 
@@ -50,7 +61,29 @@ System.out.println(usernum + "^" + usernum1 + Math.pow(usernum, usernum1));
 	
 	public static void problem04() {
 		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MIN_VALUE;
+		Scanner inKey1 = new Scanner(System.in);
+		int userNum = 1;
+		boolean not0 = true;
 		
+		while (not0) {
+			System.out.println("Choose a integer, choose 0 when your done");
+			userNum = inKey1.nextInt();
+			if (userNum == 0) {
+				not0 = false;
+			}
+			else {
+				if (userNum > max) {
+					max = userNum;
+				}
+				else if (userNum < min) {
+					min = userNum;
+				}
+			}
+		System.out.println(max);
+			System.out.print(min + "\n");
+		}
 		
 	}
 	
